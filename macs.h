58b11f608d7c2d9e83c242546c756987d87e0bb4
rwtaber <rwtaber@gmail.com>
2015-07-16 19:17:10 -0400

    Create macs.h

---
+++ macs.h
@@ -0,0 +1,10 @@
+#ifndef _MACS_H_
+#define _MACS_H_
+
+//XBeeAddress64 "XBee name" = XBeeAddress64("SH", "SL");
+
+XBeeAddress64 coordinator = XBeeAddress64(0x0013A200, 0x4093D935);
+XBeeAddress64 router1 = XBeeAddress64(0x0013A200, 0x4093D970);
+XBeeAddress64 router2 = XBeeAddress64(0x0013A200, 0x40989789);
+
+#endif
